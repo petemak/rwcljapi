c43ea027877ae435d2c5cb428ddad4f2e7c8acf2
petemak <peter.makumbi@gmail.com>
2023-11-03 20:44:00 +0100

    Added testcontainer tests

--- test/pjm/rwcapi/persistence/db_test.clj
+++ test/pjm/rwcapi/persistence/db_test.clj
@@ -13,19 +13,29 @@
     (let [pw "db-pass"
           postgres (-> (tc/create {:image-name    "postgres:15.4"
                                    :exposed-ports [5432]
-                                   :env-vars      {"POSTGRES_PASSWORD" pw}}))]
+                                   :env-vars      {"POSTGRES_PASSWORD" pw}})
+                       (tc/bind-filesystem! {:host-path      "/tmp"
+                                             :container-path "/opt"
+                                             :mode           :read-only})
+                       (tc/start!))]
 
       (try
-        (tc/start! postgres)
+
         (let [datasource (jdbc/get-datasource {:dbtype   "postgresql"
                                                :dbname   "postgres"
                                                :user     "postgres"
                                                :password pw
                                                :host     (:host postgres)
                                                :port     (get (:mapped-ports postgres) 5432)})]
-             (is (= [{:x 1}]
-                    (with-open [connection (jdbc/get-connection datasource)]
-                      (jdbc/execute! connection ["SELECT 1 as x;"])))))
+
+          (is (= "localhost" (:host postgres)))
+
+          (is (= [{:server_version "15.4 (Debian 15.4-2.pgdg120+1)"}]
+                 (jdbc/execute! datasource ["SHOW server_version;"])))
+          
+          (is (= [{:x 1}]
+                 (with-open [connection (jdbc/get-connection datasource)]
+                   (jdbc/execute! connection ["SELECT 1 as x;"])))))
         (finally
           (tc/stop! postgres))))))
 
@@ -33,25 +43,4 @@
 ; -------------------------------------------------------
 ; test containers
 ; -------------------------------------------------------
-(deftest db-integration-test
-  (testing "A simple PostgreSQL integration test"
-    (let [pw "db-pass"
-          postgres (-> (tc/create {:image-name    "postgres:15.4"
-                                   :exposed-ports [5432]
-                                   :env-vars      {"POSTGRES_PASSWORD" pw}}))]
-      
-      (try
-        (tc/start! postgres)
-        (let [datasource (jdbc/get-datasource {:dbtype   "postgresql"
-                                               :dbname   "postgres"
-                                               :user     "postgres"
-                                               :password pw
-                                               :host     (:host postgres)
-                                               :port     (:port postgres)
-                                               })]
-          (is (= [{:one 1 :tw 2}]
-                 (with-open [connection (jdbc/get-connection datasource)]
-                   (jdbc/execute! connection ["SELECT 1 ONE, 2 TWO"])))))
-        (finally
-         (tc/stop! postgres))))
-    ))
+
